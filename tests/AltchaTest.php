1cc228c6c439d71a0519ef1556f862b07c8337ee
Alexander Hofbauer <a.hofbauer@fify.at>
2025-03-03 16:30:19 +0100

    Improve tests, add coverage

--- tests/AltchaTest.php
+++ tests/AltchaTest.php
@@ -5,18 +5,21 @@ use AltchaOrg\Altcha\BaseChallengeOptions;
 use AltchaOrg\Altcha\Challenge;
 use AltchaOrg\Altcha\ChallengeOptions;
 use AltchaOrg\Altcha\Hasher\Algorithm;
+use AltchaOrg\Altcha\Hasher\Hasher;
 use AltchaOrg\Altcha\Solution;
 use PHPUnit\Framework\TestCase;
 
 class AltchaTest extends TestCase
 {
-    private static Altcha $altcha;
+    private static Hasher    $hasher;
+    private static Altcha    $altcha;
     private static Challenge $challenge;
 
     public static function setUpBeforeClass(): void
     {
         // build a default challenge for all tests (for performance reasons)
-        self::$altcha = new Altcha('test-key');
+        self::$hasher = new Hasher();
+        self::$altcha = new Altcha('test-key', self::$hasher);
         self::$challenge = self::$altcha->createChallenge();
     }
 
@@ -58,6 +61,18 @@ class AltchaTest extends TestCase
         self::assertGreaterThan(0, $solution->took);
     }
 
+    public function testInvalidSolveChallenge(): void
+    {
+        $solution = self::$altcha->solveChallenge(
+            'asd',
+            self::$challenge->salt,
+            Algorithm::from(self::$challenge->algorithm),
+            self::$challenge->maxnumber
+        );
+
+        self::assertNull($solution);
+    }
+
     public function testVerifySolution(): void
     {
         $solution = self::$altcha->solveChallenge(
@@ -77,33 +92,54 @@ class AltchaTest extends TestCase
             'number' => $solution->number,
         ];
 
-        $isValid = self::$altcha->verifySolution($payload);
+        $isValid = self::$altcha->verifySolution(base64_encode(json_encode($payload) ?: ''));
 
         self::assertTrue($isValid);
     }
 
+    public function testVerifyServerSignature(): void
+    {
+        $algorithm = Algorithm::SHA256;
+        $expires = (new \DateTimeImmutable())->add(new \DateInterval('PT10S'));
+        $verificationData = 'verified=1&expire=' . $expires->getTimestamp();
+
+        $hash = self::$hasher->hash($algorithm, $verificationData);
+        $signature = self::$hasher->hashHmacHex($algorithm, $hash, 'test-key');
+
+        $result = self::$altcha->verifyServerSignature([
+            'algorithm' => $algorithm->value,
+            'verificationData' => $verificationData,
+            'signature' => $signature,
+            'verified' => true,
+        ]);
+
+        self::assertTrue($result->verified);
+    }
+
     public function testVerifyCustomOptions(): void
     {
-        $altcha = new Altcha('my-key');
-        $challenge = $altcha->createChallenge(new ChallengeOptions(
-            algorithm: Algorithm::SHA1,
-            maxNumber: 100,
+        $maxNumber = 100;
+        $algorithm = Algorithm::SHA1;
+
+        $challenge = self::$altcha->createChallenge(new ChallengeOptions(
+            algorithm: $algorithm,
+            maxNumber: $maxNumber,
             expires: (new \DateTimeImmutable())->add(new \DateInterval('PT10S')),
             params: ['custom_param' => '123'],
             saltLength: 3,
         ));
 
-        $solution = $altcha->solveChallenge(
+        $solution = self::$altcha->solveChallenge(
             $challenge->challenge,
             $challenge->salt,
-            Algorithm::SHA1,
-            100,
+            $algorithm,
+            $maxNumber,
         );
 
         self::assertInstanceOf(Solution::class, $solution);
 
-        $isValid = $altcha->verifySolution([
-            'algorithm' => Algorithm::SHA1->value,
+        $isValid = self::$altcha->verifySolution([
+            'algorithm' => $algorithm->value,
             'challenge' => $challenge->challenge,
             'salt' => $challenge->salt,
             'signature' => $challenge->signature,
@@ -113,12 +149,72 @@ class AltchaTest extends TestCase
         self::assertTrue($isValid);
     }
 
-    public function testInvalidPayload(): void
+    public function testHandlesExpired(): void
     {
+        $maxNumber = 100;
+        $expires = (new \DateTimeImmutable())->sub(new \DateInterval('PT10S'));
+
+        $challenge = self::$altcha->createChallenge(new ChallengeOptions(
+            maxNumber: $maxNumber,
+            expires: $expires,
+        ));
+
+        $solution = self::$altcha->solveChallenge(
+            $challenge->challenge,
+            $challenge->salt,
+            Algorithm::from($challenge->algorithm),
+            $maxNumber,
+        );
+
+        self::assertInstanceOf(Solution::class, $solution);
+
+        $isValid = self::$altcha->verifySolution([
+            'algorithm' => $challenge->algorithm,
+            'challenge' => $challenge->challenge,
+            'salt' => $challenge->salt,
+            'signature' => $challenge->signature,
+            'number' => $solution->number,
+        ]);
+
+        self::assertFalse($isValid);
+    }
+
+    public function testInvalidPayloads(): void
+    {
+        // completely invalid string
         $isValid = self::$altcha->verifySolution('I am invalid');
         self::assertFalse($isValid);
 
+        // invalid base64 string
+        $isValid = self::$altcha->verifySolution('$' . base64_encode('I am invalid'));
+        self::assertFalse($isValid);
+
+        // invalid JSON string
+        $isValid = self::$altcha->verifySolution(base64_encode('I am invalid'));
+        self::assertFalse($isValid);
+
+        // invalid JSON data
+        $isValid = self::$altcha->verifySolution(base64_encode('"I am not an array"'));
+        self::assertFalse($isValid);
+
+        $isValid = self::$altcha->verifySolution([
+            'algorithm' => 'md5',
+            'challenge' => 'invalid',
+            'salt' => 'salt',
+            'signature' => 'signature',
+            'number' => 10,
+        ]);
+        self::assertFalse($isValid);
+
         $verification = self::$altcha->verifyServerSignature('I am invalid');
+        self::assertFalse($verification->verified);
+
+        $verification = self::$altcha->verifyServerSignature([
+            'algorithm' => 'md5',
+            'verificationData' => 'asd',
+            'signature' => 'signature',
+            'verified' => true,
+        ]);
         self::assertFalse($verification->verified);
     }
 }
